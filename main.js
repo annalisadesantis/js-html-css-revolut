3bc82aaabdcf4d9c8d65a78e1d7e8cdcd393aba9
Annalisa <annalisadesantis770@gmail.com>
2020-10-28 17:32:53 +0100

    RIMOZIONE CLASSI DOPPIE + MOUSE JS

--- main.js
+++ main.js
@@ -1,59 +1,21 @@
 $(document).ready(function() {
 
 
-   //  // menu-funz------------
-   //  $('.menu-funz').mouseenter(function() {
-   //     $('.dropdown-menu-funz').addClass('active');
-   // });
-   //
-   // $('.dropdown-menu-funz').mouseleave(function() {
-   //     $('.dropdown-menu-funz').removeClass('active');
-   // });
-   //
-   //
-   //  // menu-abb------------
-   //  $('.menu-abb').mouseenter(function() {
-   //     $('.dropdown-menu-abb').addClass('active');
-   // });
-   //
-   // $('.dropdown-menu-abb').mouseleave(function() {
-   //     $('.dropdown-menu-abb').removeClass('active');
-   // });
-   //
-   //
-   //  // menu-chisiamo------------
-   //  $('.menu-chisiamo').mouseenter(function() {
-   //     $('.dropdown-menu-chisiamo').addClass('active');
-   // });
-   //
-   // $('.dropdown-menu-chisiamo').mouseleave(function() {
-   //     $('.dropdown-menu-chisiamo').removeClass('active');
-   // });
-   //
-   //  // menu-aiuto------------
-   //  $('.menu-aiuto').mouseenter(function() {
-   //     $('.dropdown-menu-aiuto').addClass('active');
-   // });
-   //
-   // $('.dropdown-menu-aiuto').mouseleave(function() {
-   //     $('.dropdown-menu-aiuto').removeClass('active');
-   // });
-
-   // Soluzione 2-----------------------
-   $('.menu-funz').click(function() {
-       $('.dropdown-menu-funz').toggleClass('active');
+    // Soluzione 1--------------------
+    $('.menu-app').mouseenter(function() {
+        $('.dropdown-menu.active').removeClass('active');
+        $(this).next(".dropdown-menu").addClass('active');
    });
 
-   $('.menu-abb').click(function() {
-       $('.dropdown-menu-abb').toggleClass('active');
+   $('.dropdown-menu').mouseleave(function() {
+       $(this).removeClass('active');
    });
 
-   $('.menu-chisiamo').click(function() {
-       $('.dropdown-menu-chisiamo').toggleClass('active');
-   });
+   
+   // // // Soluzione 2-----------------------
+   // $('.menu-app').click(function() {
+   //     $(this).next(".dropdown-menu").toggleClass('active');
+   // });
 
-   $('.menu-aiuto').click(function() {
-       $('.dropdown-menu-aiuto').toggleClass('active');
-   });
 
 });
