8ee331a4413fa33f881792d0910b9ac594b63f6f
Annalisa <annalisadesantis770@gmail.com>
2020-10-28 15:25:17 +0100

    menu a tendina 3 elementi

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
 
+
+    // menu-abb------------
+    $('.menu-abb').mouseenter(function() {
+       $('.dropdown-menu-abb').addClass('active');
+   });
+
+   $('.dropdown-menu-abb').mouseleave(function() {
+       $('.dropdown-menu-abb').removeClass('active');
+   });
+
+
+    // menu-chisiamo------------
+    $('.menu-chisiamo').mouseenter(function() {
+       $('.dropdown-menu-chisiamo').addClass('active');
+   });
+
+   $('.dropdown-menu-chisiamo').mouseleave(function() {
+       $('.dropdown-menu-chisiamo').removeClass('active');
+   });
+
+    // menu-aiuto------------
+    $('.menu-aiuto').mouseenter(function() {
+       $('.dropdown-menu-aiuto').addClass('active');
+   });
+
+   $('.dropdown-menu-aiuto').mouseleave(function() {
+       $('.dropdown-menu-aiuto').removeClass('active');
+   });
+
 });
