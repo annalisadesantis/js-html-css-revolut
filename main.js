1231d2e6743afdffc72ada2fa447704d9bf00809
Annalisa <annalisadesantis770@gmail.com>
2020-10-28 15:48:52 +0100

    versione con click

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
 $(document).ready(function() {
 
 
-    // menu-abb------------
-    $('.menu-abb').mouseenter(function() {
-       $('.dropdown-menu-abb').addClass('active');
+   //  // menu-funz------------
+   //  $('.menu-funz').mouseenter(function() {
+   //     $('.dropdown-menu-funz').addClass('active');
+   // });
+   //
+   // $('.dropdown-menu-funz').mouseleave(function() {
+   //     $('.dropdown-menu-funz').removeClass('active');
+   // });
+   //
+   //
+   //  // menu-abb------------
+   //  $('.menu-abb').mouseenter(function() {
+   //     $('.dropdown-menu-abb').addClass('active');
+   // });
+   //
+   // $('.dropdown-menu-abb').mouseleave(function() {
+   //     $('.dropdown-menu-abb').removeClass('active');
+   // });
+   //
+   //
+   //  // menu-chisiamo------------
+   //  $('.menu-chisiamo').mouseenter(function() {
+   //     $('.dropdown-menu-chisiamo').addClass('active');
+   // });
+   //
+   // $('.dropdown-menu-chisiamo').mouseleave(function() {
+   //     $('.dropdown-menu-chisiamo').removeClass('active');
+   // });
+   //
+   //  // menu-aiuto------------
+   //  $('.menu-aiuto').mouseenter(function() {
+   //     $('.dropdown-menu-aiuto').addClass('active');
+   // });
+   //
+   // $('.dropdown-menu-aiuto').mouseleave(function() {
+   //     $('.dropdown-menu-aiuto').removeClass('active');
+   // });
+
+   // Soluzione 2-----------------------
+   $('.menu-funz').click(function() {
+       $('.dropdown-menu-funz').toggleClass('active');
    });
 
-   $('.dropdown-menu-abb').mouseleave(function() {
-       $('.dropdown-menu-abb').removeClass('active');
+   $('.menu-abb').click(function() {
+       $('.dropdown-menu-abb').toggleClass('active');
    });
 
-
-    // menu-chisiamo------------
-    $('.menu-chisiamo').mouseenter(function() {
-       $('.dropdown-menu-chisiamo').addClass('active');
+   $('.menu-chisiamo').click(function() {
+       $('.dropdown-menu-chisiamo').toggleClass('active');
    });
 
-   $('.dropdown-menu-chisiamo').mouseleave(function() {
-       $('.dropdown-menu-chisiamo').removeClass('active');
-   });
-
-    // menu-aiuto------------
-    $('.menu-aiuto').mouseenter(function() {
-       $('.dropdown-menu-aiuto').addClass('active');
-   });
-
-   $('.dropdown-menu-aiuto').mouseleave(function() {
-       $('.dropdown-menu-aiuto').removeClass('active');
+   $('.menu-aiuto').click(function() {
+       $('.dropdown-menu-aiuto').toggleClass('active');
    });
 
 });
